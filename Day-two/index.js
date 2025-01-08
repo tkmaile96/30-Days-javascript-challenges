c6c098cb61466b72029d43fdd435225b8be82442
Teekay <tkmaile96@gmail.com>
2025-01-08 08:49:56 +0200

    Updated the date function

--- Day-two/index.js
+++ Day-two/index.js
@@ -4,16 +4,25 @@ const date = document.getElementById('date');
 
 //Date function
 function updateDate() {
+ const today = new Date("01/08/2025");
 
+ const DD = today.getDate();
+ const MM = today.getMonth() + 1;
+ const YYYY = today.getFullYear();
+
+ date.textContent = `${DD}/${MM}/${YYYY}`;
 
 }
 
+updateDate();
+
 //Clock function
 function updateClock() {
-    const today = new Date;
+    const today = new Date();
     const HH = today.getHours();
-    const MM = today.getMinutes();  
+    const MM = today.getMinutes();
     const SS = today.getSeconds();
+
 
     clock.textContent = `${HH}:${MM}:${SS}`;
 
